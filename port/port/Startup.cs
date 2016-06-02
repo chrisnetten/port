bf21706c3e2a87b1949381cf11ee1b21ad355c73
chris netten <200230280@MyGeorgian.ca>
2016-06-01 22:39:49 -0400

    start up

---
+++ port/port/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(port.Startup))]
+namespace port
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
